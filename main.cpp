4c0ba84724aa282ce813ce4ca5ff541a388f1965
Jeppe Bøgeskov <67188645+Jepbe@users.noreply.github.com>
2022-12-15 16:02:06 +0100

    summery

--- main.cpp
+++ main.cpp
@@ -3,8 +3,12 @@ using namespace std;
 
 int main ()
 {
-    string thing;
-    cout << "Type something: ";
-    cin >> thing;
-    cout << "You typed: " << thing;
+    int num1, num2;
+    int sum;
+    cout << "type a number: ";
+    cin >> num1;
+    cout << "type another number: ";
+    cin >> num2;
+    sum = num1 + num2;
+    cout << "Sum is: " << sum;
 }
