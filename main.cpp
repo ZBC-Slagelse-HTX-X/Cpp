796e0d332bb8d24ecbc6a14b5ef47c9d2d80185d
Jeppe Bøgeskov <67188645+Jepbe@users.noreply.github.com>
2022-12-15 14:10:17 +0100

    gofy aaah program

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@ using namespace std;
 
 int main ()
 {
-    cout << "Hello, World! \n";
+    int number = 69;
+    cout << number;
+    string newString = "\nHello, World!\n";
+    cout << newString;
+
+    int num1 = 2;
+    int num2 = 2;
+    int sum = num1 + num2;
+    cout << sum;
     return 0;
 }
