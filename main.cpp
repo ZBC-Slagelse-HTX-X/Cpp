48f8235d57714ef383dde00d9fcf55c7c7ebc50f
Jeppe Bøgeskov <67188645+Jepbe@users.noreply.github.com>
2022-12-15 15:42:36 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,8 @@ using namespace std;
 
 int main ()
 {
-    int number = 69;
-    cout << number;
-    string newString = "\nHello, World!\n";
-    cout << newString;
-
-    int num1 = 2;
-    int num2 = 2;
-    int sum = num1 + num2;
-    cout << sum;
-    return 0;
+    string thing;
+    cout << "Type something: ";
+    cin >> thing;
+    cout << "You typed: " << thing;
 }
